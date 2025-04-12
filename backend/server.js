bf63eb3fc9b7b5fb98a18ee16278e4d85c50e53f
ItzNivetha <149505904+ItzNivetha@users.noreply.github.com>
2025-04-12 21:02:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Nivetha@Yuvi',
+  password: '',
   database: 'sign_language',
   waitForConnections: true,
   connectionLimit: 10,
